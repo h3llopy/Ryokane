66e430610d80dc6c238adbe356b376e5fc98fe66
Wafi Chaar <wafi.chaar@bas.sarl>
2020-07-06 00:02:47 +0300

    order shop products by website_sequence asc

--- custom_number_of_product_cols/controllers/main.py
+++ custom_number_of_product_cols/controllers/main.py
@@ -1,3 +1,9 @@
-from odoo.addons.website_sale.controllers import main
+from odoo.addons.website_sale.controllers.main import WebsiteSale
 main.PPG = 18 # Products per Page
 main.PPR = 3 # Products per Row
+
+class WebsiteSale(WebsiteSale):
+def _get_search_order(self, post):
+        # OrderBy will be parsed in orm and so no direct sql injection
+        # id is added to be sure that order is a unique sort key
+        return 'is_published desc, %s, id desc, website_sequence ASC' % post.get('order')
