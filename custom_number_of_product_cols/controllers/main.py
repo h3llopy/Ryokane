f86ce1c9d94186baa6e9beb8d0c21e51d25143b3
Wafi Chaar <wafi.chaar@bas.sarl>
2020-07-06 00:23:56 +0300

    put back PPR=3

--- custom_number_of_product_cols/controllers/main.py
+++ custom_number_of_product_cols/controllers/main.py
@@ -2,8 +2,3 @@ from odoo.addons.website_sale.controllers import main
 main.PPG = 18 # Products per Page
 main.PPR = 3 # Products per Row
 
-class WebsiteSale(WebsiteSale):
-def _get_search_order(self, post):
-        # OrderBy will be parsed in orm and so no direct sql injection
-        # id is added to be sure that order is a unique sort key
-        return 'is_published desc, %s, id desc, website_sequence ASC' % post.get('order')
