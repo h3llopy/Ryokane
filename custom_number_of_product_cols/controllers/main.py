5f4ad22d996647d5156a866a081099405cecbfce
Wafi Chaar <wafi.chaar@bas.sarl>
2020-07-06 00:12:11 +0300

    fix product per row in eshop

--- custom_number_of_product_cols/controllers/main.py
+++ custom_number_of_product_cols/controllers/main.py
@@ -1,4 +1,4 @@
-from odoo.addons.website_sale.controllers.main import WebsiteSale
+from odoo.addons.website_sale.controllers import main
 main.PPG = 18 # Products per Page
 main.PPR = 3 # Products per Row
 
