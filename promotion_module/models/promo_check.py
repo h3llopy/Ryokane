798a9d6d78d635575316ff991cb65855480c7050
Babatope Ajepe <ajepebabatope@gmail.com>
2020-08-22 21:23:28 +0100

    Update promo_check.py

--- promotion_module/models/promo_check.py
+++ promotion_module/models/promo_check.py
@@ -20,7 +20,7 @@ class PromoClass(models.Model):
         for program in self:
             if program.reward_type == 'product' and \
                not order.order_line.filtered(lambda line: line.product_id == program.reward_product_id):
-                order.write({'order_line': [(0, False, value) for value in order._put_reward_values_product(program)]})
+                order.write({'order_line': [(0, value) for value in order._put_reward_values_product(program)]})
             elif program.reward_type == 'discount' and program.discount_apply_on == 'specific_product' and \
                not order.order_line.filtered(lambda line: line.product_id == program.discount_specific_product_id):
                 continue
