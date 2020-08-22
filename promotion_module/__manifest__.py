baf615d929414d140989ed117ff8ae0f31a93c58
Babatope Ajepe <ajepebabatope@gmail.com>
2020-08-22 21:23:52 +0100

    Update __manifest__.py

--- promotion_module/__manifest__.py
+++ promotion_module/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': "Sale Promotion",
-    'version': '12.0.1.0.2',
+    'version': '12.0.1.0.3',
     'summary': """Create Promotion Offers For Sales""",
     'description': """This Module Allows to Set  Promotion Offers On Products And Product Categories.""",
     'author': "BAS",
