c8c68e17ed40d59d134f311046549a9a07ca5365
Wafi Chaar <wafi.chaar@bas.sarl>
2020-08-27 21:41:05 +0300

    promo_check v20

--- promotion_module/__manifest__.py
+++ promotion_module/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': "Sale Promotion",
-    'version': '12.0.1.0.19',
+    'version': '12.0.1.0.20',
     'summary': """Create Promotion Offers For Sales""",
     'description': """This Module Allows to Set  Promotion Offers On Products And Product Categories.""",
     'author': "BAS",
